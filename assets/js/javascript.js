cb2c22c42ad0e9743ed89d6ce58e6474aaa36242
BrunoCezarGuimaraes <bruno_guimaraes007@hotmail.com>
2021-12-31 13:31:17 -0300

    Movimento das teclas

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -11,12 +11,34 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
     var jogo = {}
 
+    /*Teclas*/
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+    });
+
+
     //Game Loop
     jogo.timer = setInterval(loop, 30);
 
     function loop() {
 
         movefundo();
+        movejogador();
 
     } // Fim da função loop()
 
@@ -29,6 +51,28 @@ function start() { // Inicio da função start()
         $("#fundoGame").css("background-position", esquerda - 1);
 
     } // fim da função movefundo()
+
+    function movejogador() {
+
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+
+        }
+
+        if (jogo.pressionou[TECLA.S]) {
+
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+        }
+
+        if (jogo.pressionou[TECLA.D]) {
+
+            //Chama função Disparo
+        }
+
+    } // fim da função movejogador()
+
 
 
 
