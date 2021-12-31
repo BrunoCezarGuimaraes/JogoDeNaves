2bb37dac8dad2bebcba1f233876e0889af736753
BrunoCezarGuimaraes <bruno_guimaraes007@hotmail.com>
2021-12-31 13:53:09 -0300

    bloqueio do player

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -58,12 +58,24 @@ function start() { // Inicio da função start()
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo - 10);
 
+
+            if (topo <= 0) {
+
+                $("#jogador").css("top", topo + 10);
+            }
+
         }
 
         if (jogo.pressionou[TECLA.S]) {
 
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo + 10);
+
+            if (topo >= 434) {
+                $("#jogador").css("top", topo - 10);
+
+            }
+
         }
 
         if (jogo.pressionou[TECLA.D]) {
