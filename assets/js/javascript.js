107fbecbffeda7e3b62efeb52be9218e0006e446
BrunoCezarGuimaraes <bruno_guimaraes007@hotmail.com>
2021-12-31 14:02:17 -0300

    Movimentação dos outros elementos

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -11,6 +11,10 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
     var jogo = {}
 
+    /*Movimentação do inimigo*/
+    var velocidade = 5;
+    var posicaoY = parseInt(Math.random() * 334);
+
     /*Teclas*/
     var TECLA = {
         W: 87,
@@ -39,6 +43,9 @@ function start() { // Inicio da função start()
 
         movefundo();
         movejogador();
+        moveinimigo1();
+        moveinimigo2();
+        moveamigo();
 
     } // Fim da função loop()
 
@@ -85,6 +92,43 @@ function start() { // Inicio da função start()
 
     } // fim da função movejogador()
 
+    function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+
+        }
+    } //Fim da função moveinimigo1()
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left", posicaoX - 3);
+
+        if (posicaoX <= 0) {
+
+            $("#inimigo2").css("left", 775);
+
+        }
+    } // Fim da função moveinimigo2()
+
+    function moveamigo() {
+
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left", posicaoX + 1);
+
+        if (posicaoX > 906) {
+
+            $("#amigo").css("left", 0);
+
+        }
+
+    } // fim da função moveamigo()
 
 
 
