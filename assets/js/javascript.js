51b95f0ed56b639f54509c241c6ecd43695307b7
BrunoCezarGuimaraes <bruno_guimaraes007@hotmail.com>
2021-12-31 15:29:44 -0300

    pontuação

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,11 +6,17 @@ function start() { // Inicio da função start()
     $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    $("#fundoGame").append("<div id='placar'></div>");
 
 
     //Principais variáveis do jogo
     var jogo = {}
     var fimdejogo = false;
+
+    /*Pontos*/
+    var pontos = 0;
+    var salvos = 0;
+    var perdidos = 0;
 
     /*Movimentação do inimigo*/
     var velocidade = 5;
@@ -51,6 +57,7 @@ function start() { // Inicio da função start()
         moveinimigo2();
         moveamigo();
         colisao();
+        placar();
 
     } // Fim da função loop()
 
@@ -207,6 +214,7 @@ function start() { // Inicio da função start()
         // Disparo com o inimigo1
         if (colisao3.length > 0) {
 
+            pontos = pontos + 100;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -222,6 +230,7 @@ function start() { // Inicio da função start()
         // Disparo com o inimigo2
         if (colisao4.length > 0) {
 
+            pontos = pontos + 50;
             inimigo2X = parseInt($("#inimigo2").css("left"));
             inimigo2Y = parseInt($("#inimigo2").css("top"));
             $("#inimigo2").remove();
@@ -236,6 +245,7 @@ function start() { // Inicio da função start()
         // jogador com o amigo
         if (colisao5.length > 0) {
 
+            salvos++;
             reposicionaAmigo();
             $("#amigo").remove();
         }
@@ -244,6 +254,7 @@ function start() { // Inicio da função start()
         //Inimigo2 com o amigo
         if (colisao6.length > 0) {
 
+            perdidos++;
             amigoX = parseInt($("#amigo").css("left"));
             amigoY = parseInt($("#amigo").css("top"));
             explosao3(amigoX, amigoY);
@@ -351,5 +362,11 @@ function start() { // Inicio da função start()
         }
 
     } // Fim da função explosao3
+
+    function placar() {
+
+        $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
+
+    } //fim da função placar()
 
 } // Fim da função start
