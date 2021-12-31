0c0bb1aefe2b5af0b1a4ab28d5ad0d2f6b6b984c
BrunoCezarGuimaraes <bruno_guimaraes007@hotmail.com>
2021-12-31 16:42:38 -0300

    GameOver

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,16 +10,16 @@ function start() { // Inicio da função start()
     $("#fundoGame").append("<div id='energia'></div>");
 
 
-    //Principais variáveis do jogo
-    var jogo = {}
-    var fimdejogo = false;
+    /*Principais variáveis do jogo - Nota: ao por var acontece um bug que não permite ler o valor dentro da variavel*/
+    jogo = {}
+    fimdejogo = false;
 
     /*Pontos*/
-    var pontos = 0;
-    var salvos = 0;
-    var perdidos = 0;
+    pontos = 0;
+    salvos = 0;
+    perdidos = 0;
 
-    /*Pontos - Nota: ao por var acontece um bug que não permite ler o valor dentro da variavel*/
+    /*Pontos/
     energiaAtual = 3;
 
     /*Movimentação do inimigo*/
@@ -426,6 +426,28 @@ function energia() {
         $("#energia").css("background-image", "url(assets/imgs/energia0.png)");
 
         //Game Over
+        gameOver();
     }
+
+
+    //Função GAME OVER
+    function gameOver() {
+        fimdejogo = true;
+        musica.pause();
+        somGameover.play();
+
+        window.clearInterval(jogo.timer);
+        jogo.timer = null;
+
+        $("#jogador").remove();
+        $("#inimigo1").remove();
+        $("#inimigo2").remove();
+        $("#amigo").remove();
+
+        $("#fundoGame").append("<div id='fim'></div>");
+
+        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+    } // Fim da função gameOver();
+
 
 } // Fim da função energia()
